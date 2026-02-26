9142e2679e57920716601b7bd720b1fec56a48ea
sus0303 <susanahlopez3@gmail.com>
2026-02-26 10:22:13 -0500

    Initialize Streamlit app with basic features

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from PIL import Image 
+
+st.title("Esta es mi primera app en la nube")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('cloud.jpg')
+st.image(image, caption='Interfaces multimodales')
+                   
+texto = st.text_input('escribe algo','este es mi texto')
+st.write('el texto escrito es', texto)
